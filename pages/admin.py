f1bfc14f5ca2ddeb3f9d6b4faf56746adb1da9e9
idrice24 <idrice.tsafouet@yahoo.com>
2022-04-29 19:25:48 +0100

    add files #11

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Question
+from .models import Choice
+
+admin.site.register(Question) # this will help us to modify the models in the admins side board
+admin.site.register(Choice)
