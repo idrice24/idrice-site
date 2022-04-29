a68027ac39c3f6e21049da9ca41863ffe520e304
idrice24 <idrice.tsafouet@yahoo.com>
2022-04-29 19:27:09 +0100

    update db.sqlite3 #8

--- idrice_site/urls.py
+++ idrice_site/urls.py
@@ -13,11 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+
+
+
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('home/', include("pages.urls")),
+    path('pages/', include("pages.urls")),
     path('admin/', admin.site.urls),
     
 ]
